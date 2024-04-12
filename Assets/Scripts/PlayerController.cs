182ac2ffaa5c5ac4844d8c1caef749aedc479664
nayan <nayankamalverma@gmail.com>
2024-04-12 14:52:58 +0530

    Added code to play crouch anomation when ctrl is pressed and code to accept vertical input and vertical > 0 then play jump animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,15 +15,35 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        HorizontalMovement();
+        VerticalMovement();
+    }
+
+    void HorizontalMovement()
+    {
         float speed = Input.GetAxisRaw("Horizontal");
         animator.SetFloat("Speed", Mathf.Abs(speed));
-        Vector3 scale= transform.localScale;
+        Vector3 scale = transform.localScale;
         //move left
-        if (speed < 0){
+        if (speed < 0)
+        {
             scale.x = -1f * Mathf.Abs(scale.x);
-        }else if (speed > 0) {
+        }
+        else if (speed > 0)
+        {
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+    }
+    void VerticalMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            animator.SetBool("crouch", true);
+        }
+        float verticalInput = Input.GetAxis("Vertical");
+        if (verticalInput > 0) { 
+            animator.SetBool("jump",true);
+        }
     }
 }
