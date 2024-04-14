aa7cbb454d8273ca0dc9d801577e5fe295e35183
nayan <nayankamalverma@gmail.com>
2024-04-15 00:23:36 +0530

    added code for player movement (horizontal) and done some change in structure of code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,47 +4,69 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //componets
     [SerializeField]
     private Animator animator;
     [SerializeField]
     private BoxCollider2D playerCollider;
-    // Start is called before the first frame update
+    private Rigidbody2D rd;
 
+    //data to update player
+    [SerializeField]
+    private float playerSpeed;
+    [SerializeField]
+    private float jumpForce;
     private Vector2 playerColl_Size;//for storing initial size of collider
     private Vector2 playerColl_Offset;//for storing initial offset of collider
     void Start()
     {
         playerColl_Size = playerCollider.size;
         playerColl_Offset = playerCollider.offset;
+        rd = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        HorizontalMovement();
-        VerticalMovement();
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        PlayerMovementAnimation(horizontalInput, verticalInput);
+        PlayerMovement(horizontalInput, verticalInput);
     }
 
-    void HorizontalMovement()
+    void PlayerMovement(float horizontalInput, float verticalInput)
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(speed));
+        //horizontal movement
+        Vector2 position = transform.position;
+        position.x += horizontalInput * playerSpeed * Time.deltaTime;
+        transform.position = position;
+
+        //vertcal movement
+        if (verticalInput > 0)
+        {
+            rd.AddForce(new Vector2(0, jumpForce), ForceMode2D.Force);
+        }
+    }
+    void PlayerMovementAnimation(float horizontalInput, float verticalInput)
+    {
+        //horizontal
+        animator.SetFloat("Speed", Mathf.Abs(horizontalInput));
         Vector3 scale = transform.localScale;
         //move left
-        if (speed < 0)
+        if (horizontalInput < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
         }
-        else if (speed > 0)
+        else if (horizontalInput > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
-    }
-    void VerticalMovement()
-    {
-        if (Input.GetKeyDown(KeyCode.LeftControl))
-        {
+
+        //vertical jump and crouch
+
+        if (Input.GetKeyDown(KeyCode.LeftControl)){
+
             float sizeX = 0.6048745f;       //size x
             float sizeY = 1.49f;            //size y
 
@@ -52,21 +74,22 @@ public class PlayerController : MonoBehaviour
             float offY = 0.68f;           //Offset y
 
             animator.SetBool("crouch", true);
-            playerCollider.size = new Vector2(sizeX, sizeY);   
+            playerCollider.size = new Vector2(sizeX, sizeY);
             playerCollider.offset = new Vector2(offX, offY);
 
-        }else if(Input.GetKeyUp(KeyCode.LeftControl))
-        {
+        }else if (Input.GetKeyUp(KeyCode.LeftControl)){
             animator.SetBool("crouch", false);
             playerCollider.offset = playerColl_Offset;
-            playerCollider.size = playerColl_Size; 
+            playerCollider.size = playerColl_Size;
         }
-        float verticalInput = Input.GetAxis("Vertical");
-        if (verticalInput > 0) { 
-            animator.SetBool("jump",true);
-        }else if (verticalInput < 0)
+
+        //jump
+        if (verticalInput > 0)
         {
-            animator.SetBool("jump",false);
+            animator.SetBool("jump", true);
+        }else
+        {
+            animator.SetBool("jump", false);
         }
     }
 }
