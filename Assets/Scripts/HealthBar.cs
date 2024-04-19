5f935d84ec1f7020744c2a62d8e50a41889540f0
nayan <nayankamalverma@gmail.com>
2024-04-20 02:19:14 +0530

    added player health system and reload level on player death

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerHealth playerHealth;
+    [SerializeField]
+    private Image totalHealthBar;
+    [SerializeField]
+    private Image currentHealthBar;
+
+    private void Start()
+    {
+        totalHealthBar.fillAmount = playerHealth.CurrentHealth / 10;
+    }
+
+    private void Update()
+    {
+        currentHealthBar.fillAmount = playerHealth.CurrentHealth / 10;
+    }
+}
